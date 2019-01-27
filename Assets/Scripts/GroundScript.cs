63f83023df5779d5358a1c5c29dab1fc32f5cebc
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-01-26 20:19:33 -0800

    Add yshape

--- Assets/Scripts/GroundScript.cs
+++ Assets/Scripts/GroundScript.cs
@@ -5,36 +5,41 @@ using UnityEngine;
 
 public class GroundScript : MonoBehaviour
 {
-    //public GameObject cube;
     public List<GameObject> shapeList;
     public GameObject verticalShapePrefab;
     public GameObject verticalShape;
+    public GameObject yShapePrefab;
+    public GameObject yShape;
+    public GameObject currShape;
 
     // Start is called before the first frame update
     void Start()
     {
+
         verticalShape = Instantiate(verticalShapePrefab, new Vector3(0, 0.5f, 0), Quaternion.AngleAxis(90, Vector3.right));
-        //shape1.transform.position = new Vector3(0, 0.5f, 0);
+        yShape = Instantiate(yShapePrefab, new Vector3(0, 0.5f, 0), Quaternion.AngleAxis(90, Vector3.right));
         shapeList = new List<GameObject>();
         shapeList.Add(verticalShape);
+        shapeList.Add(yShape);
 
         Debug.Log(verticalShape.transform);
+        currShape = verticalShape;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        verticalShape.transform.Translate(Vector3.back * Time.deltaTime, Space.World);
+        currShape.transform.Translate(Vector3.back * Time.deltaTime, Space.World);
 
-        var pos = verticalShape.transform.position;
+        var pos = currShape.transform.position;
         Debug.Log(pos);
 
         if (pos.z < -11)
         {
-            shapeList.Remove(verticalShape);
-            Destroy(verticalShape);
-            verticalShape = shapeList[shapeList.Count - 1];
+            shapeList.Remove(currShape);
+            Destroy(currShape);
+            currShape = shapeList[shapeList.Count - 1];
         }
 
     }
